eb74a8f65454201b84160d123f9fa9e38e1928a4
Mvcart <marcos_cota@usp.br>
2026-08-19 11:34:43 -0300

    refactor(game): defer mine placement and validation to start() method

--- backend/game/game.py
+++ backend/game/game.py
@@ -13,20 +13,15 @@ from backend.exceptions import GameAlreadyEndedError, InvalidMoveError, InvalidC
 class Game:
     # Public methods
     def __init__(self, 
-                 width: int, 
-                 height: int, 
-                 mine_count: int, 
-                 first_click_x: int, 
-                 first_click_y: int,
+                 width: int,
+                 height: int,
+                 mine_count: int,
                 #  safe_zone_strategy_class: Optional[SafeZoneStrategy] = None, # TO IMPLEMENT IN THE FUTURE
                  placement_strategy_class: Optional[MinePlacementStrategy] = None,
                  seed: Optional[int] = None,
                  ) -> None:
         if mine_count < 0:
             raise InvalidConfigurationError("Mine count cannot be negative.")
-        # The first click cant be a mine
-        if not self._valid_first_click(width, height, first_click_x, first_click_y):
-            raise InvalidConfigurationError("Invalid first click coordinates.")
         
         if seed is None:
             seed = secrets.randbits(64)
@@ -37,11 +32,6 @@ class Game:
         self.height = height
         self.board = Board(width, height)
         self.mine_count = mine_count
-        safe_cells = self._get_safe_zone(first_click_x, first_click_y)
-
-        max_mines = (width * height) - len(safe_cells)
-        if mine_count > max_mines:
-            raise InvalidConfigurationError(f"Too many mines for this board size. Max mines: {(max_mines)}")
 
         self.id = str(uuid.uuid4())
 
@@ -52,24 +42,39 @@ class Game:
             
         self.placement_strategy = placement_strategy_class(seed = self.seed)
 
+        self.state = GameState.WAITING
+        self.start_time: Optional[datetime] = None
+        self.end_time: Optional[datetime] = None
+
+    def start(self, first_click_x: int, first_click_y: int) -> None:
+        # The first click can't be outside the board
+        if not self._valid_first_click(first_click_x, first_click_y):
+            raise InvalidConfigurationError("Invalid first click coordinates.")
+            
+        safe_cells = self._get_safe_zone(first_click_x, first_click_y)
+        max_mines = (self.width * self.height) - len(safe_cells)
+        if self.mine_count > max_mines:
+            raise InvalidConfigurationError(f"Too many mines for this board size. Max mines: {(max_mines)}")
+
         mine_positions = self.placement_strategy.place_mines(
-            width,
-            height,
-            mine_count,
-            safe_cells,)
+            self.width,
+            self.height,
+            self.mine_count,
+            safe_cells)
         
         for x, y in mine_positions:
             self.board.place_mine_at(x, y)
         
-        self.board.reveal_cell(first_click_x, first_click_y)
-
         self.state = GameState.PLAYING
-        self.start_time: Optional[datetime] = datetime.now()
-        self.end_time: Optional[datetime] = None
+
+        self.click(first_click_x, first_click_y)
+        self.start_time = datetime.now()
 
     # Click/reveal func. updates game state
     def click(self, x: int, y: int) -> None:
-        if self.state != GameState.PLAYING:
+        if self.state == GameState.WAITING:
+            self.start(x, y)
+        elif self.state != GameState.PLAYING:
             raise GameAlreadyEndedError(f"Game is already {self.state.value}.")
         
         hit_mine = self.board.reveal_cell(x, y)
@@ -98,7 +103,7 @@ class Game:
         data["start_time"] = self.start_time.isoformat() if self.start_time else None
         data["end_time"] = self.end_time.isoformat() if self.end_time else None
         data["board"] = self.board.to_dict()
-        if self.state == GameState.PLAYING:
+        if self.state == GameState.PLAYING or self.state == GameState.WAITING:
             for line in data["board"]:
                 for cell in line:
                     if not cell["is_revealed"]:
@@ -106,6 +111,11 @@ class Game:
                         cell["neighbor_mines"] = None
 
         return data
+
+    @staticmethod
+    def get_absolute_max_mines(width: int, height: int) -> int:
+        # worst-case scenario
+        return (width * height) - 1
 
     # Private methods
     # Maybe outsource this func like the mine placement?
@@ -119,11 +129,11 @@ class Game:
 
         return safe_zone
 
-    def _valid_first_click(self, width: int, height: int, x: int, y: int) -> bool:
-        if x < 0 or x >= width:
+    def _valid_first_click(self, x: int, y: int) -> bool:
+        if x < 0 or x >= self.width:
             return False
         
-        if y < 0 or y >= height:
+        if y < 0 or y >= self.height:
             return False
         
         return True
