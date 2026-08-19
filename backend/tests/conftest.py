b20698cf7d847081d3c9fb3cee4bfacb8bbe8186
Mvcart <marcos_cota@usp.br>
2026-08-19 12:00:26 -0300

    test(game): adjust tests for WAITING state and start() method

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -13,5 +13,28 @@ from ..game.game import Game
     | * | 1 | 0 | 1 | * |
 """
 @pytest.fixture
-def seeded_game_42() -> Game:
-    return Game(5, 5, 3, 2, 2, seed=42)
+def seeded_game_42_waiting() -> Game:
+    return Game(
+        width=5,
+        height=5,
+        mine_count=3,
+        seed=42)
+
+@pytest.fixture
+def first_click_coordinates_seeded_game_42() -> dict:
+    return {
+        "x": 2,
+        "y": 2
+        }
+
+@pytest.fixture
+def seeded_game_42_playing(
+    first_click_coordinates_seeded_game_42,
+    seeded_game_42_waiting
+    ) -> Game:
+    game = seeded_game_42_waiting
+    game.start(
+        first_click_x = first_click_coordinates_seeded_game_42["x"],
+        first_click_y = first_click_coordinates_seeded_game_42["y"]
+        )
+    return game
--- backend/tests/test_game.py
+++ backend/tests/test_game.py
@@ -7,8 +7,15 @@ from ..game.gamestate import GameState
 from ..exceptions import InvalidConfigurationError, GameAlreadyEndedError
 
 class TestGameState:
-    def test_valid_initial_state(self, seeded_game_42):
-        game = seeded_game_42
+    def test_valid_initial_state(self, seeded_game_42_waiting, first_click_coordinates_seeded_game_42):
+        game = seeded_game_42_waiting
+
+        assert game.state == GameState.WAITING
+
+        game.start(
+            first_click_coordinates_seeded_game_42["x"],
+            first_click_coordinates_seeded_game_42["y"]
+        )
 
         assert game.state == GameState.PLAYING
 
@@ -26,15 +33,15 @@ class TestGameState:
         
         assert n_mines == 3
 
-    def test_loose_condition(self, seeded_game_42):
-        lost_game = seeded_game_42
+    def test_loose_condition(self, seeded_game_42_playing):
+        lost_game = seeded_game_42_playing
 
         lost_game.click(4,0)
         assert lost_game.board.get_cell(4, 0).is_mine
         assert lost_game.state == GameState.LOST
 
-    def test_win_condition(self, seeded_game_42):
-        won_game = seeded_game_42
+    def test_win_condition(self, seeded_game_42_playing):
+        won_game = seeded_game_42_playing
 
         assert not won_game.board.get_cell(0, 2).is_mine
         assert not won_game.board.get_cell(0, 1).is_mine
@@ -44,8 +51,8 @@ class TestGameState:
         won_game.click(0, 4)
         assert won_game.state == GameState.WON
 
-    def test_flag(self, seeded_game_42):
-        game = seeded_game_42
+    def test_flag(self, seeded_game_42_playing):
+        game = seeded_game_42_playing
 
         assert not game.board.get_cell(0, 2).is_revealed
         game.flag(0, 2)
@@ -53,19 +60,40 @@ class TestGameState:
     
     def test_negative_mine_count(self):
         with pytest.raises(InvalidConfigurationError, match=re.escape("Mine count cannot be negative.")):
-            Game(1, 1, -2, 0, 0, seed=42)
+            Game(
+                width=1,
+                height=1,
+                mine_count=-2,
+                seed=42
+                )
         
     def test_invalid_first_click(self):
         with pytest.raises(InvalidConfigurationError, match=re.escape("Invalid first click coordinates.")):
-            Game(5, 5, 2, -1, 0, seed=42)
+            Game(
+                width=5,
+                height=5,
+                mine_count=2,
+                seed=42
+                ).start(
+                    first_click_x=-1,
+                    first_click_y=0
+                )
     
     def test_too_may_mines(self):
         with pytest.raises(InvalidConfigurationError, match=re.escape("Too many mines for this board size. Max mines: 16")):
-            Game(5, 5, 25, 2, 2, seed=42)
+            Game(
+                width=5,
+                height=5,
+                mine_count=25,
+                seed=42
+                ).start(
+                    first_click_x=2,
+                    first_click_y=2
+                )
 
-    def test_click_after_game_over(self, seeded_game_42):
+    def test_click_after_game_over(self, seeded_game_42_playing):
         with pytest.raises(GameAlreadyEndedError, match=re.escape("Game is already lost.")):
-            lost_game = seeded_game_42
+            lost_game = seeded_game_42_playing
 
             lost_game.click(4,0)
             assert lost_game.board.get_cell(4, 0).is_mine
@@ -73,9 +101,9 @@ class TestGameState:
 
             lost_game.click(2, 2)
 
-    def test_flag_after_game_over(self, seeded_game_42):
+    def test_flag_after_game_over(self, seeded_game_42_playing):
         with pytest.raises(GameAlreadyEndedError, match=re.escape("Game is already lost.")):
-            lost_game = seeded_game_42
+            lost_game = seeded_game_42_playing
 
             lost_game.click(4,0)
             assert lost_game.board.get_cell(4, 0).is_mine
@@ -83,8 +111,8 @@ class TestGameState:
 
             lost_game.flag(4, 0)
 
-    def test_to_dict_lost(self, seeded_game_42):
-        lost_game = seeded_game_42
+    def test_to_dict_lost(self, seeded_game_42_playing):
+        lost_game = seeded_game_42_playing
 
         dictionary = lost_game.to_dict()
 
@@ -119,8 +147,8 @@ class TestGameState:
                 assert dictionary["board"][y][x]["is_mine"] == lost_game.board.get_cell(x, y).is_mine
                 assert dictionary["board"][y][x]["neighbor_mines"] == lost_game.board.get_cell(x, y).neighbor_mines
 
-    def test_to_dict_won(self, seeded_game_42):
-        won_game = seeded_game_42
+    def test_to_dict_won(self, seeded_game_42_playing):
+        won_game = seeded_game_42_playing
 
         dictionary = won_game.to_dict()
 
